6ea4eae7e103dd75370cb80a7d8ea21793aac54a
guilhem13 <guilhem.maille@gmail.com>
2022-03-17 14:03:17 +0100

    switch to cermine

--- knowledgegraph/controller/pipeline/mainprocess.py
+++ knowledgegraph/controller/pipeline/mainprocess.py
@@ -31,65 +31,39 @@ class Pipeline():
             processor = Textprocessed(data.link) #before  data.link[0]        
             text_processed = processor.get_data_from_pdf()
             data.entities_include_in_text = processor.find_entities_in_raw_text()
-            """try: 
-                 data.entities_from_reference = service_two_extraction.ServiceTwo(str("file/"+data.doi[0]+".pdf")).get_references()
+            try: 
+                 data.entities_from_reference = service_two_extraction.ServiceTwo(str("knowledgegraph/file/"+data.doi+".pdf")).get_references()
             except Exception as e:
                 print("Service two didn't work ")
                 print(e)
-                data.entities_from_reference =service_one_extraction.ServiceOne(text_processed).get_references() """
-            #data.entities_from_reference = service_one_extraction.ServiceOne(text_processed).get_references()
-            #data.entities_from_reference = service_two_extraction.ServiceTwo(str("file/"+data.doi[0]+".pdf")).get_references()
-            #a = service_one_extraction.ServiceOne(text_processed).get_references()
-            #a = service_two_extraction.ServiceTwo(str("file/"+data.doi[0]+".pdf")).get_references()
-            #b = [ x.__dict__ for x in a ]
-            #data.entities_from_reference = b
-            #data.entities_from_reference = service_one_extraction.ServiceOne(text_processed).get_references() 
-            print("taille du texte ")
-            print(len(data.entities_from_reference))
-            #print(len(text_processed))"""
-            #data.entities_from_reference = processor.find_entites_based_on_regex(text_processed)
-            entities_from_regex = processor.find_entites_based_on_regex(text_processed)
-            entities_from_serviceone = service_one_extraction.ServiceOne(text_processed).get_references()
-            final_entities_list =[]
-            final_entities_list += entities_from_regex
-            #print(len(entities_from_regex))
-            #print(len(entities_from_serviceone))
-            if len(entities_from_regex) >0 :
-                if len(entities_from_serviceone)>0:
-                    """for i in range(len(entities_from_regex)): 
-                        j = 0
-                        stop = False 
-                        while (j < len(entities_from_serviceone)):# or (stop == False):
-                            if entities_from_serviceone[j].__eq__(entities_from_regex[i])==False:
-                                final_entities_list.append(entities_from_serviceone[j])
-                                stop = True
-                            j = j+1
-                        
-                        #final_entities_list.append(entities_from_serviceone[i])"""
-                    
-                    for i in range(len(entities_from_serviceone)): 
-                        stop =False
-                        j = 0
-                        while (j < (len(entities_from_regex)-1)):
-                            if stop ==False: 
-                                if entities_from_serviceone[i].__eq__(entities_from_regex[j])==True:
-                                    stop =True
-                            j+=1
-                        if stop == False: 
-                            final_entities_list.append(entities_from_serviceone[i])
+                print("taille du texte ")
+                entities_from_regex = processor.find_entites_based_on_regex(text_processed)
+                entities_from_serviceone = service_one_extraction.ServiceOne(text_processed).get_references()
+                final_entities_list =[]
+                final_entities_list += entities_from_regex
+                if len(entities_from_regex) >0 :
+                    if len(entities_from_serviceone)>0:                        
+                        for i in range(len(entities_from_serviceone)): 
+                            stop =False
+                            j = 0
+                            while (j < (len(entities_from_regex)-1)):
+                                if stop ==False: 
+                                    if entities_from_serviceone[i].__eq__(entities_from_regex[j])==True:
+                                        stop =True
+                                j+=1
+                            if stop == False: 
+                                final_entities_list.append(entities_from_serviceone[i])
 
-                    data.entities_from_reference =  entities_from_regex
-                else:
-                    data.entities_from_reference =  entities_from_regex
-            else: 
-                data.entities_from_reference =  entities_from_serviceone 
+                        data.entities_from_reference =  entities_from_regex
+                    else:
+                        data.entities_from_reference =  entities_from_regex
+                else: 
+                    data.entities_from_reference =  entities_from_serviceone 
                 
-            data.entities_from_reference = final_entities_list #entities_from_regex #final_entities_list
+                data.entities_from_reference = final_entities_list 
             data.url_in_text = processor.find_url_in_text()
-            data.doi_in_text = processor.find_doi_in_text()#frfr
-            data.date_published = str(data.date_published) # TODO a enlever c'était pour le test de json
-            #before  data.link[0] #TODO dans le cas où l'on est dans le service 2
-            #os.remove(str("knowledge/file/"+data.doi+".pdf")) 
+            data.doi_in_text = processor.find_doi_in_text()
+            data.date_published = str(data.date_published) 
             out_queue.put(data)
 
     
